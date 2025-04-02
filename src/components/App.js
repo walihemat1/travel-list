4977f33efd92f553d4882005b978588a0c0b3c3a
Ahmad Wali Hemat <ahmadwalihemat67@gmail.com>
2025-04-02 21:46:05 +0430

    add all files

---
+++ src/components/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearItemsList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div>
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItems={handleToggleItem}
+        onClearItems={handleClearItemsList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/notes.md
@@ -0,0 +1,41 @@
+# Thinking in React:
+
+# The "Thinking in React" Process:
+
+# 1. Break the desired UI into components and establish the component tree. This also include thinking about reuseability and composibility of components.
+
+# 2. Build a static version in React without any state and interactivity.
+
+# 3. Think about state:
+
+# 👉 When to use state
+
+# 👉 Types of state: local vs global
+
+# 👉 Where to place each piece of state
+
+# 4. Establish data flow:
+
+# 👉 One-way data flow
+
+# 👉 Child-to-parent communication
+
+# 👉 Accessing global state
+
+# Step 3 and 4 are called state management.
+
+# When you know how to "Think In React", you will be able to answer:
+
+# 🤔 How to break up UI Design into components?
+
+# 🤔 How to make some components reusable?
+
+# 🤔 How to assemble UI from reusable components?
+
+# 🤔 What pieces of state do I need for interactivity?
+
+# 🤔 Where to place state? (What component should "own" each piece of state?)
+
+# 🤔 What types of state can or should I use?
+
+# 🤔 How to make data flow through app?
